aa4281e1b79063de83fc4a6c9a2501a2734df9a3
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2026-04-29 16:46:15 +1200

    storing input form information in localstorage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { viewChild } from '@angular/core';
+import { afterNextRender } from '@angular/core';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -8,6 +11,33 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  
+  private form =viewChild.required<NgForm>('form')
+  private destroyref=inject(DestroyRef)
+
+  constructor(){
+  afterNextRender(()=>{
+
+  const savedForm=window.localStorage.getItem('saved-login-form')
+
+  if(savedForm){
+    const loadedFormData=JSON.parse(savedForm);
+    const savedEmail=loadedFormData.email;
+    
+    setTimeout(()=>{
+    this.form().controls['email'].setValue(savedEmail);
+    },1)
+
+
+  }
+
+    const subscription= this.form()?.valueChanges?.subscribe({
+    next:(value)=>window.localStorage.setItem('saved-login-form',JSON.stringify({email:value.email}))
+   });
+   this.destroyref.onDestroy(()=> subscription?.unsubscribe())
+  })
+  }
+
   onSubmit(formData:NgForm){
   // console.log(form);
   if(!formData.form.valid){
@@ -17,5 +47,6 @@ export class LoginComponent {
     const enteredpassword=formData.form.value.password
 
     console.log(enteredEmail,enteredpassword);
+    formData.form.reset();
   }
 }
