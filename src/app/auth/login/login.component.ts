844a10195a55f72e70e8be861d821aabcddc4dc4
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2026-04-30 16:57:17 +1200

    Connecting and registering inputs for complex form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,52 +1,27 @@
-import { Component, DestroyRef, inject, ViewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { viewChild } from '@angular/core';
-import { afterNextRender } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports:[FormsModule],
+  imports:[ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  
-  private form =viewChild.required<NgForm>('form')
-  private destroyref=inject(DestroyRef)
-
-  constructor(){
-  afterNextRender(()=>{
-
-  const savedForm=window.localStorage.getItem('saved-login-form')
-
-  if(savedForm){
-    const loadedFormData=JSON.parse(savedForm);
-    const savedEmail=loadedFormData.email;
-    
-    setTimeout(()=>{
-    this.form().controls['email'].setValue(savedEmail);
-    },1)
-
-
-  }
-
-    const subscription= this.form()?.valueChanges?.subscribe({
-    next:(value)=>window.localStorage.setItem('saved-login-form',JSON.stringify({email:value.email}))
-   });
-   this.destroyref.onDestroy(()=> subscription?.unsubscribe())
+  form = new FormGroup({
+    email:new FormControl('',{
+      validators:[Validators.email,Validators.required],
+    }),
+    password:new FormControl('')
   })
+  
+  get emailIsInvalid(){
+    return this.form.controls.email.valid && this.form.controls.password.dirty
   }
 
-  onSubmit(formData:NgForm){
-  // console.log(form);
-  if(!formData.form.valid){
-    return
-  }
-  const enteredEmail=formData.form.value.email
-    const enteredpassword=formData.form.value.password
+  onSubmit(){
 
-    console.log(enteredEmail,enteredpassword);
-    formData.form.reset();
   }
+
 }
